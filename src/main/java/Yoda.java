ab388efeaaaeb6a843d156a14aea439ab65914b1
M4ttL4m <mattworkingonly@gmail.com>
2026-08-22 14:23:34 +0800

    Add Level-0: Rename chatbot to Yoda, add greeting and exit messages
    - Rename main class from Duke to Yoda
    - Add custom ASCII banner art for Yoda
    - Implement basic greeting and exit messages with visual separators
    - Update README.md and AGENTS.md documentation accordingly

---
+++ src/main/java/Yoda.java
@@ -0,0 +1,20 @@
+
+public class Yoda {
+
+    public static void main(String[] args) {
+        String separator = "____________________________________________________________";
+        String banner = "__   __  ___  ____    _\n"
+                + "\\ \\ / / / _ \\|  _ \\  / \\\n"
+                + " \\ V / | | | | | | |/ _ \\\n"
+                + "  | |  | |_| | |_| / ___ \\\n"
+                + "  |_|   \\___/|____/_/   \\_\\\n";
+
+        System.out.println(separator);
+        System.out.print(banner);
+        System.out.println("Hello! I'm YODA.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
+    }
+}
