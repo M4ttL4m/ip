569198db4b27802b90acae909c435b30b07f1600
M4ttL4m <mattworkingonly@gmail.com>
2026-08-28 01:52:40 +0800

    Level-1 & 2 & 3
    Level 1. Echo
    Level 2. Add, List
    Level 3. (Un)Mark as Done

---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+/**
+ * Represents one task in the user's task list.
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone(){
+        this.isDone = true;
+    }
+
+    public void markAsUndone(){
+        this.isDone = false;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "[" + (isDone ? "X" : " ") + "] " + description;
+    }
+}
--- src/main/java/Yoda.java
+++ src/main/java/Yoda.java
@@ -1,4 +1,9 @@
 
+import java.util.Scanner;
+
+/**
+ * Starts the YODA command-line application.
+ */
 public class Yoda {
 
     public static void main(String[] args) {
@@ -11,10 +16,58 @@ public class Yoda {
 
         System.out.println(separator);
         System.out.print(banner);
+        System.out.println(separator);
         System.out.println("Hello! I'm YODA.");
         System.out.println("What can I do for you?");
         System.out.println(separator);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(separator);
+
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(separator);
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(separator);
+                break;
+            }
+            else if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            }
+            else if (command.startsWith("mark ")) {
+
+                int taskIndex = Integer.parseInt(command.substring(5)) - 1;
+
+                tasks[taskIndex].markAsDone();
+
+                System.out.println("     Nice! I've marked this task as done:");
+                System.out.println("       " + tasks[taskIndex]);
+            }
+            else if (command.startsWith("unmark ")) {
+
+                int taskIndex = Integer.parseInt(command.substring(7)) - 1;
+
+                tasks[taskIndex].markAsUndone();
+
+                System.out.println("     Nice! I've marked this task as not done yet:");
+                System.out.println("       " + tasks[taskIndex]);
+            }
+            else {
+                tasks[taskCount] = new Task(command);
+                taskCount++;
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            }
+
+
+            System.out.println(separator);
+        }
     }
 }
